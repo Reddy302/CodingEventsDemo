c3cce5d382876906e60690a4de8acaf8a3c37a66
Reddy Lewis <lewismd3283@gmail.com>
2021-02-14 03:36:12 -0600

    just before 15.4 Validating Models in a Controller

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,79 @@
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using CodingEvents.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Event> events = new List<Event>(EventData.GetAll());
+            return View(events);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddEventViewModel addEventViewModel = new AddEventViewModel();
+            return View(addEventViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddEventViewModel addEventViewModel)
+        {
+            Event newEvent = new Event
+            {
+                Name = addEventViewModel.Name,
+                Description = addEventViewModel.Description,
+                ContactEmail = addEventViewModel.ContactEmail
+            };
+
+            EventData.Add(newEvent);
+            return Redirect("/events");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach(int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+            return Redirect("/events");
+        }
+
+        [Route("/events/edit/{eventId}")]
+        public IActionResult Edit(int eventId)
+        {
+            // controller code will go here
+            Event evt = EventData.GetById(eventId);
+            ViewBag.eventToEdit = evt;
+            ViewBag.Title = $"Edit Event {evt.Name} (id={evt.Id})";
+            return View();
+        }
+
+        [HttpPost, Route("/events/edit")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            // controller code will go here
+            Event evt = EventData.GetById(eventId);
+            evt.Name = name;
+            evt.Description = description;
+            return Redirect("/events");
+        }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Models
+{
+    public class Event
+    {
+        public int Id { get; }
+        private static int nextId = 1;
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ContactEmail { get; set; }
+
+        public Event()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Event(string name, string description, string contactEmail) : this()
+        {
+            Name = name;
+            Description = description;
+            ContactEmail = contactEmail;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Event @event &&
+                   Id == @event.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
